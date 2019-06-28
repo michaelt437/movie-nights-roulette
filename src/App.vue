96ea132fbed1ef6da14742b041e0d24e4ea95d2b
Michael Tran <mtran@dealeron.com>
2019-06-28 17:04:01 -0400

    testing pr

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,6 @@
       <router-link to="/about">About</router-link>
     </div> -->
     <router-view/>
-    <a href="https://github.com/michaelt437/movie-nights-roulette" class="text-teal text-lg self-center"><i class="fab fa-github" title="Careful, it's a flerken..."></i></a>
+    <a href="https://github.com/michaelt437/movie-nights-roulette" class="text-teal text-lg self-center"><i class="fab fa-github" title="Careful, it's a flarken..."></i></a>
   </div>
 </template>
