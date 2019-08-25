baa8ac621bc6a995f8ad379811722a9222e40ff0
michaelt437 <michaelt437@gmail.com>
2019-08-25 19:19:00 -0400

    deploy messages

--- postDeploy.js
+++ postDeploy.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var xhr = new XMLHttpRequest();
 let msg = JSON.stringify({
-  "text": `:niccage::dancing_granny: A new Roulette update has been published! :dancing_bob::niccage:`,
+  "text": `:niccage::dancing_granny: A new Roulette update has been published! :dancing_bob::niccage:\nadded IMDB to streaming list\nincreased short pick length threshold to 107 minutes`,
 })
 
 xhr.open("POST", process.env.DEPLOY_HOOK)
